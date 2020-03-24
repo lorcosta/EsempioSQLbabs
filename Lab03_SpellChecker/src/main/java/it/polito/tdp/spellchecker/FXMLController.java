a9392f32b5c811516436b7ac1ed3c7ab1bf93337
lorenzocosta <lorenzocosta@mbp-di-lorenzo>
2020-03-24 12:48:49 +0100

    Implementata logica del model

--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/FXMLController.java
@@ -6,8 +6,12 @@ package it.polito.tdp.spellchecker;
 
 import it.polito.tdp.spellchecker.model.Dictionary;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -15,7 +19,9 @@ import javafx.scene.control.TextArea;
 import javafx.scene.text.Text;
 
 public class FXMLController{
-
+	ObservableList<String> list=FXCollections.observableArrayList();
+	Dictionary d= new Dictionary();
+	
     @FXML
     private ResourceBundle resources;
 
@@ -23,8 +29,8 @@ public class FXMLController{
     private URL location;
 
     @FXML
-    private ChoiceBox<?> boxChoice;
-
+    private ChoiceBox<String> choiceDictionary;
+    
     @FXML
     private TextArea txtInput;
 
@@ -42,20 +48,46 @@ public class FXMLController{
 
     @FXML
     private Text txtTime;
+    
+    @FXML
+    void doChooseLanguage() {//Carico il dizionario corretto
+    	String language=choiceDictionary.getValue();
+    	if(language==null)
+    		txtOutput.setText("Scegliere quale dizionario utilizzare");
+    	else d.loadDictionary(language);
+    }
 
     @FXML
     void doClearText(ActionEvent event) {
-
+    	txtInput.clear();
+    	txtOutput.clear();
     }
 
     @FXML
     void doSpellCheck(ActionEvent event) {
-
+    	this.doChooseLanguage();
+    	//Prendere tutte le parole inserite e trasformarle in una lista
+    	String inputText=txtInput.getText().replaceAll("\\p{P}", "").toLowerCase();
+    	List<String> inputTextList=new ArrayList<String>();
+    	for(String s: inputText.split(" ")) {
+    		inputTextList.add(s);
+    	}
+    	//Passare le parole inserite al model per poterle controllare 
+    	d.spellCheckText(inputTextList);
+    	//Ricevere la correzione delle parole
+    	//Mostrare le parole scorrette
+  
     }
 
+    private void loadData() {
+    	String italian="Italian";
+    	String english="English";
+    	list.addAll(italian,english);
+    	choiceDictionary.getItems().addAll(list);
+    }
     @FXML
     void initialize() {
-        assert boxChoice != null : "fx:id=\"boxChoice\" was not injected: check your FXML file 'Scene.fxml'.";
+    	loadData();
         assert txtInput != null : "fx:id=\"txtInput\" was not injected: check your FXML file 'Scene.fxml'.";
         assert btnSpellCheck != null : "fx:id=\"btnSpellCheck\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtOutput != null : "fx:id=\"txtOutput\" was not injected: check your FXML file 'Scene.fxml'.";
--- Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
+++ Lab03_SpellChecker/src/main/java/it/polito/tdp/spellchecker/model/Dictionary.java
@@ -4,20 +4,22 @@ import java.io.*;
 import java.util.*;
 
 public class Dictionary {
-	
+	HashSet<String> dictionary= new HashSet<String>();
 	/**
 	 * Carica il dizionario della lingua prescelta.
 	 * @param language
 	 */
 	public void loadDictionary(String language) {
-	
+		String nomeFile="src/main/resources/"+language+".txt";
 		try {
-			FileReader fr= new FileReader("");
+			FileReader fr= new FileReader(nomeFile);
 			BufferedReader br= new BufferedReader(fr);
 			String word;
 			while((word=br.readLine())!=null) {
 				//Aggiungere una parola alla struttura dati
+				dictionary.add(word);
 			}
+			System.out.println("Caricato il dizionario");
 			br.close();
 		}catch(IOException e) {
 			System.out.println("Errore nella lettura del file");
@@ -31,6 +33,13 @@ public class Dictionary {
 	 * @return List<RichWord>
 	 */
 	public List<RichWord> spellCheckText(List<String> inputTextList){
-		return null;
+		List<RichWord> correzioni= new ArrayList<RichWord>();
+		for(String s: inputTextList) {
+			if(dictionary.contains(s))
+				correzioni.add(new RichWord(s,true));
+			else correzioni.add(new RichWord(s,false));
+				
+		}
+		return correzioni;
 	}
 }
