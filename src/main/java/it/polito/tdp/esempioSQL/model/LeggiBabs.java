b4e9c41e9a8f9f91bd6fa81149a72dbf3e31ba00
lorenzocosta <lorenzocosta@mbp-di-lorenzo>
2020-03-26 17:03:22 +0100

    Prima lezione JDBC

---
+++ src/main/java/it/polito/tdp/esempioSQL/model/LeggiBabs.java
@@ -0,0 +1,46 @@
+package it.polito.tdp.esempioSQL.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class LeggiBabs {
+	public void run() {
+		String jdbcURL="jdbc:mysql://localhost/babs?user=root&password=mortaccimia";
+		try {
+			Connection conn=DriverManager.getConnection(jdbcURL);//DriverManager è una classe statica
+			//Significa che posso chiamarne i metodi senza dover creare un'istanza della classe
+			Statement st= conn.createStatement();
+			
+			String sql="SELECT NAME FROM station";
+			
+			ResultSet res=st.executeQuery(sql); 
+			
+			while(res.next()) {
+				String nomeStazione=res.getString("NAME");
+				System.out.println(nomeStazione);
+			}
+			st.close();
+			
+			Statement st2= conn.createStatement();
+			
+			//Codice 
+			
+			conn.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		//FACTORY: creazione di un oggetto di una classe, 
+		//senza conoscere il tipo della classe (non potevo usare NEW)
+		//uso un metodo fornito da un'altra classe che internamente fa la NEW
+		//e conoscerà il tipo di classe effettivo
+		
+	}
+	public static void main(String args[]) {
+		LeggiBabs babs=new LeggiBabs();
+		babs.run();
+	}
+}
