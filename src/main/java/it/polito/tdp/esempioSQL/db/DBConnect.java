acda1ecf8c29ea245c45493a9c792db558d13e97
lorenzocosta <lorenzocosta@mbp-di-lorenzo>
2020-04-02 11:49:32 +0200

    upload

--- src/main/java/it/polito/tdp/esempioSQL/db/DBConnect.java
+++ src/main/java/it/polito/tdp/esempioSQL/db/DBConnect.java
@@ -7,7 +7,7 @@ import java.sql.SQLException;
 public class DBConnect {
 
 	public static Connection getConnection() throws SQLException {
-		String jdbcURL="jdbc:mysql://localhost/babs?user=root&password=mortaccimia";
+		String jdbcURL="jdbc:mysql://localhost/babs?user=root&password=root";
 		return DriverManager.getConnection(jdbcURL);
 	}
 }
